555f7369cd981d02af250d63d3ba7f0dd11a50db
Florent Bartoli <florent.bartoli@sygnum.com>
2022-11-16 12:12:56 +0100

    clean up

--- contracts/HoF.sol
+++ contracts/HoF.sol
@@ -6,10 +6,10 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 /*
-  ___                         ___
- (o o)                       (o o)
-(  V  ) SYGNUM HALL OF FAME (  V  )
---m-m-------------------------m-m--
+            ___                         ___
+            (o o)                       (o o)
+            (  V  ) SYGNUM HALL OF FAME (  V  )
+            --m-m-------------------------m-m--
 */
 /// @custom:security-contact florent.bartoli@sygnum.com
 contract HoF is ERC1155, Ownable, ERC1155Supply {
--- subgraph/src/factory.ts
+++
@@ -1,8 +0,0 @@
-import { SygnumERC1155 } from "../generated/templates";
-import { NewCollection } from "../generated/NFTfactory/NFTFactory";
-
-export function NewCollectionHandler(event: NewCollection): void {
-  // Start indexing the exchange; `event.params.exchange` is the
-  // address of the new exchange contract
-  SygnumERC1155.create(event.params._proxyAddress);
-}
